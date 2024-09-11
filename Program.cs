9d67ba3a71506c24ed95f685e5b4a916ce8a6dd7
Ave Pajo <ave.pajo@tktk.ee>
2024-09-12 00:55:26 +0300

    add my first app

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/* Ave esimene programm
+ Mitme reaga kommentaar See https://aka.ms/new-console-template for more information */
+// ühe reaga kommentaar
+
+using System.Security.Cryptography.X509Certificates;
+
+string myfirstName = "Ave";
+string mylastName = "Pajo";
+//myName = "Pajo";
+
+//proov
+//string eesnimi;
+//eesnimi = "Ave";
+
+//Console.WriteLine("Tere " + myfirstName + " " + mylastName + "!"+eesnimi);
+
+Console.WriteLine($"Tere, {myfirstName} {mylastName}!");
